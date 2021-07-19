2b171ab4b6bb8a6b87b26ad868e108eed4d696d5
Tchi <86352901+LucasVbr@users.noreply.github.com>
2021-07-19 23:21:33 +0200

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -2,20 +2,21 @@ function generatePassword(nbDigits, caps, numbers, specials) {
     var resultat = ""
     
     for (let i  = 0; i < nbDigits ; i++) {
-        resultat += generateCharacter(caps, numbers, specials)
+        resultat += generateCharacter(true, caps, numbers, specials)
     }
 
     return resultat
 }
 
-function generateCharacter(caps, numbers, specials) {
+function generateCharacter(letters, caps, numbers, specials) {
     var LETTERS_TABLE = "abcdefghijklmnopqrstuvwxyz"
     var CAPS_TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     var NUMBERS_TABLE = "0123456789"
     var SPECIALS_TABLE = "!#$%&'()*+,-./:;=?@[]^_`{|}~";
 
-    var resultat = LETTERS_TABLE
+    var resultat = ""
 
+    if (letters) resultat += LETTERS_TABLE
     if (caps) resultat += CAPS_TABLE
     if (numbers) resultat += NUMBERS_TABLE
     if (specials) resultat += SPECIALS_TABLE
